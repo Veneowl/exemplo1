b76a0d5f75578269bc9de81b689425dd07ab371a
Veneowl <viniciusgabriel3457@gmail.com>
2025-03-13 01:44:10 +0000

    aula12032025

--- app/index.php
+++ app/index.php
@@ -1,8 +1,22 @@
 <?php
-// require './vendor/autoload.php';
+$nome = $_GET['nome'];
+$nome = $_POST['nome'];
+?>
 
-// use Application\DBConnection\MySQLConnection;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira Aplicação</title>
+</head>
+<body>
+    <h1>Olá Vinícius<?= $nome ?></h1>
+    <form action="">
+<label for="nome">Digite seu Nome</label>
+<input type="text"name= "nome"/>
+<button type="submit">Enviar</button>
 
-// $db = new MySQLConnection();
-
-echo phpinfo();
+    </form>
+</body>
+</html>
